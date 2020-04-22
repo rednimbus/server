56426fdaa996976bab454e1c678e42fb2838510e
xiangxu <blacknimbus@163.com>
2020-04-22 16:07:02 +0800

    web

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,12 @@
 var express = require('express');
 var router = express.Router();
+const fs = require('fs');
+const path = require('path');
+
+router.get('/web', function(req, res) {
+  res.set('Content-Type', 'text/html');
+  res.send(fs.readFileSync(path.join(__dirname, './web.html'), 'utf-8'));
+})
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
